f2022d072a639c39ef60eb36d52579714fc99dff
VCheng <vcheng@users.noreply.github.com>
2019-04-29 15:18:53 -0700

    updates based on group edits

--- di_loads_dt.m
+++ di_loads_dt.m
@@ -1,7 +1,6 @@
-function didt = di_loads_dt(v_buses, i_loads,  Z_loads, I_inc_loads)
+function didt = di_loads_dt(v_buses, i_loads, inv_L_loads, Z_loads, I_inc_loads)
 %This function returns the derivative of the current to loads
 %Equation (10) in Curi Paper
 
-didt = (-Z_loads*i_loads + I_inc_loads'*v_buses);
-%didt = -i_loads;
+didt = inv_L_loads*(-Z_loads*i_loads + I_inc_loads'*v_buses);
 end
